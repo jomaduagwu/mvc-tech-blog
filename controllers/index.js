d69c3296435b94cfa4e0642e3d6097a4d00cecde
Ms Eko <jomaduagwu@gmail.com>
2023-05-24 20:25:09 -0500

    import and configure routes

---
+++ controllers/index.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const apiRoutes = require('./api');
+const loginRoutes = require('./logins'); // need this?
+const homeRoutes = require('./homeroutes');
+const dashboardRoutes = require('./dashboardroutes')
+
+router.use('/api', apiRoutes);
+router.use(loginRoutes);
+router.use('/', homeRoutes);
+router.use('/dashboard', dashboardRoutes);
+
+module.exports = router;
