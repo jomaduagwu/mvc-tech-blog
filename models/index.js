069373724e1314caac76b09bde2608b2fb3a68b8
Ms Eko <jomaduagwu@gmail.com>
2023-05-30 16:33:18 -0500

    added code for models

--- models/index.js
+++ models/index.js
@@ -1,13 +1,35 @@
-const User = require('./User');
-const Project = require('./Project');
+const User = require('/User'); //should be ./?
+const Post = require('/Post');
+const Comment = require('/Comment');
 
-User.hasMany(Project, {
-  foreignKey: 'user_id',
+User.hasMany(Post, {
+  foreignKey: 'userId',
   onDelete: 'CASCADE'
 });
 
-Project.belongsTo(User, {
-  foreignKey: 'user_id'
+User.hasMany(Comment, {
+  foreignKey: 'userId',
+  onDelete: 'CASCADE'
 });
 
-module.exports = { User, Project };
+Post.belongsTo(User, {
+  foreignKey: 'userId',
+  onDelete: 'CASCADE'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'postId',
+  onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'userId'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'postId',
+  
+});
+
+
+module.exports = { User, Post, Comment };
